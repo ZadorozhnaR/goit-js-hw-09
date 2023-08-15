c375dde6b127556afa58f31f5267385747b97571
Iryna <ira.shkraba@ukr.net>
2023-08-15 11:16:30 +0300

    third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const refs = {
+  body: document.querySelector('body'),
+  form: document.querySelector('form.form'),
+  firstDelay: document.querySelector('[name="delay"]'),
+  delayStep: document.querySelector('[name="step"]'),
+  formAmount: document.querySelector('[name="amount"]'),
+};
+
+refs.form.addEventListener('click', fuctionCreatePromise);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function fuctionCreatePromise(e) {
+  e.preventDefault();
+
+  let delay = Number(refs.firstDelay.value);
+  let step = Number(refs.delayStep.value);
+  let amount = Number(refs.formAmount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    let promiseDelay = delay + step * i;
+
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
